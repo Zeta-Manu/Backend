b44ea63068f08f9c3c99ba339b0e8db28c8fb0e2
entangle2giraffe <bittersalad@tutanota.de>
2024-03-15 00:54:43 +0700

    delete old code

--- internal/api/routes/predict_routes.go
+++ internal/api/routes/predict_routes.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/Zeta-Manu/Backend/docs"
 	"github.com/Zeta-Manu/Backend/internal/adapters/database"
 	"github.com/Zeta-Manu/Backend/internal/adapters/s3"
 	"github.com/Zeta-Manu/Backend/internal/api/controllers"
@@ -14,8 +13,6 @@ import (
 func InitPredictRoutes(router *gin.Engine, dbAdapter database.DBAdapter, s3Adapter s3.S3Adapter, cfg config.AppConfig) {
 	predictController := controllers.NewPredictController(dbAdapter, s3Adapter)
 
-	// TODO: Fixed Swagger
-	docs.SwaggerInfo.BasePath = "/api"
 	user := router.Group("/api", manu_auth.AuthenticationMiddleware(cfg.JWT.JWTPublicKey))
 	{
 		user.POST("/predict", predictController.Predict)
